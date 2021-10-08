f1c5548b67cb8cf638ecfe4d5edb347638f148c4
chamara <dchamarad91@gmail.com>
2021-10-08 17:43:07 +0530

    pay method completed

--- src/app/online-payment-select/online-payment-select.page.ts
+++ src/app/online-payment-select/online-payment-select.page.ts
@@ -12,6 +12,8 @@ import payList from '../../assets/getInvoices.json';
 export class OnlinePaymentSelectPage implements OnInit {
   total: number = 0;
   bills: any = [];
+  indexes: any = [];
+  payList: any = [];
   constructor(
     private router: Router,
     public kiaProviderService: KiaProviderService,
@@ -22,15 +24,25 @@ export class OnlinePaymentSelectPage implements OnInit {
   }
 
   selectInvoce(event){
-    console.log(event)
     let isChecked = event.checked;
     let refNo = event.value.substring(0,event.value.indexOf(","));
-    let amount: number = event.value.substring(event.value.indexOf(",")+1, event.value.length);
+    let amount: number = event.value.substring(event.value.indexOf(",")+1, event.value.indexOf("+"));
+    let index: number = event.value.substring(event.value.indexOf("+")+1, event.value.length);
     if(isChecked){
       this.total = Number(this.total) + Number(amount);
+      this.indexes.push(index)
     }else{
       this.total = Number(this.total) - Number(amount);
+      this.indexes.splice(this.indexes.indexOf(index),1)
     }
-    console.log(this.total)
+    console.log(this.indexes)
+  }
+
+  pay(){
+    this.payList=[];
+    this.indexes.forEach(element => {
+      this.payList.push(this.bills[element]);
+    });
+    console.log(this.payList)
   }
 }
